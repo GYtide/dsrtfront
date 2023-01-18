0678c9050522b5ccbd603006f6af756e90804d0d
gytide <2142780749@qq.com>
2023-01-18 17:01:07 +0800

    频谱预览

--- test/es6.js
+++ test/es6.js
@@ -1,10 +1,15 @@
-
-function test(){
-
-
-    return {2,3}
+let base = +new Date(1968, 9, 3);
+let oneDay = 24 * 3600 * 1000;
+let date = [];
+let data = [Math.random() * 300];
+for (let i = 1; i < 20000; i++) {
+    var now = new Date((base += oneDay));
+    date.push([now.getFullYear(), now.getMonth() + 1, now.getDate()].join('/'));
+    data.push(Math.round((Math.random() - 0.5) * 20 + data[i - 1]));
 }
 
-let a = test()
+console.log(data)
 
-console.log(a)
+let buffer = Buffer.from(data)
+
+console.log(buffer)
