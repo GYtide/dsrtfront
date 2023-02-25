b9a518a945d2fbce41bf22ca5ca44566080c70ef
gytide <2142780749@qq.com>
2023-02-25 20:53:06 +0800

    可拖拽显示

--- test/test.js
+++ test/test.js
@@ -1,23 +1,7 @@
-// const fs = require('fs');
-// const path = require('path');
+let a = 150082
 
-import fs from 'fs';;
-import path from 'path';
-
-function countLines(dir) {
-  let count = 0;
-
-  fs.readdirSync(dir).forEach(file => {
-    const filePath = path.join(dir, file);
-
-    if (fs.statSync(filePath).isDirectory()) {
-      count += countLines(filePath);
-    } else if (filePath.endsWith('.html')) {
-      count += fs.readFileSync(filePath, 'utf-8').split('\n').length;
+for(let i = 0 ; i < Math.sqrt(a) ; ++i){
+    if(Number.isInteger(a/i)){
+        console.log(i,a/i)
     }
-  });
-
-  return count;
 }
-
-console.log(`The number of lines of code in .html files is: ${countLines('.')}`);
